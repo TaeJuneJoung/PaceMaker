b0822c4186d3c9e771f82ea2998f2644328f3b3a
TaeJuneJoung <jtj0525@gmail.com>
2019-10-14 19:25:59 +0900

    회원가입 페이지 작성중 | req1-5 | S1P3151005-7

---
+++ front/pages/MemberJoin.vue
@@ -0,0 +1,91 @@
+<template>
+  <v-container justify-center align-center fill-height="true">
+    <v-row justify="center" align="center">
+      <v-col sm="5" xs="10">
+        <v-card class="pa-2 text-center">
+          <v-flex class="headline ma-2">Join</v-flex>
+          <v-card-text>
+            <v-form ref="form" v-model="valid">
+              <v-text-field
+                v-model="email"
+                :counter="30"
+                :rules="emailRules"
+                label="Email"
+                required
+              ></v-text-field>
+              <v-text-field
+                v-model="nickname"
+                :counter="15"
+                :rules="nicknameRules"
+                label="Nickname"
+                required
+              ></v-text-field>
+              <v-text-field
+                v-model="password"
+                :counter="20"
+                :rules="passwordRules"
+                label="Password"
+                required
+                :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+                :type="show ? 'text' : 'password'"
+                @click:append="show = !show"
+                @change="checkPassword"
+              ></v-text-field>
+              <v-text-field
+                v-model="rePassword"
+                :counter="20"
+                :rules="rePasswordRules"
+                label="Confirm Password"
+                required
+                :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
+                :type="show2 ? 'text' : 'password'"
+                @click:append="show2 = !show2"
+                @change="checkPassword"
+              ></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions class="justify-end">
+            <v-btn class="ma-4" color="success">회원가입</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      valid: true,
+      email: '',
+      emailRules: [
+        (v) => !!v || 'E-mail is required',
+        (v) => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+      ],
+      nickname: '',
+      nicknameRules: [(v) => !!v || 'NickName is required'],
+      password: '',
+      passwordRules: [
+        (v) => !!v || 'Password is required',
+        (v) =>
+          (v && v.length >= 8) ||
+          v.length <= 20 ||
+          'Password must be more than 8 characters or less then 20 characters'
+      ],
+      rePassword: '',
+      rePasswordRules: [
+        (v) => !!v || 'Password is required',
+        (v) =>
+          this.password === this.rePassword || '비밀번호가 일치하지 않습니다.'
+      ],
+      show: false,
+      show2: false,
+      isPasswordOk: false
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- front/pages/index.vue
+++ front/pages/index.vue
@@ -4,34 +4,17 @@
       <v-col sm="5" xs="10">
         <v-card raised class="pa-2 text-center" color="rgba(0,0,0,0.4)">
           <v-flex class="headline ma-2">Member login</v-flex>
-          <v-icon x-large color="rgb(248, 246, 207)" class="display-4"
-            >mdi-account-supervisor</v-icon
-          >
+          <v-icon x-large color="rgb(248, 246, 207)" class="display-4">mdi-account-supervisor</v-icon>
           <v-card-text>
             <v-form ref="form">
-              <v-text-field
-                v-model="email"
-                :rules="emailRules"
-                label="E-mail"
-                solo
-                required
-              ></v-text-field>
-
-              <v-text-field
-                v-model="password"
-                :rules="passRules"
-                label="Password"
-                solo
-                required
-              ></v-text-field>
+              <v-text-field v-model="email" :rules="emailRules" label="E-mail" solo required></v-text-field>
+              <v-text-field v-model="password" :rules="passRules" label="Password" solo required></v-text-field>
             </v-form>
           </v-card-text>
           <v-card-actions class="text-xs-center">
-            <v-btn block color="rgb(248, 246, 207)" @click="validate"
-              >Login</v-btn
-            >
+            <v-btn block color="rgb(248, 246, 207)" @click="validate">Login</v-btn>
           </v-card-actions>
-          <v-btn color="rgb(248, 246, 207)" text nuxt to>회원 가입</v-btn>
+          <v-btn color="rgb(248, 246, 207)" text nuxt to="/MemberJoin">회원 가입</v-btn>
           <v-btn color="rgb(248, 246, 207)" text nuxt to>ID/PW 찾기</v-btn>
         </v-card>
       </v-col>
