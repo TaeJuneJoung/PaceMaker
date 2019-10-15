f35c13405b79cd856ec5c38b781a154393923f2e
TaeJuneJoung <jtj0525@gmail.com>
2019-10-15 19:17:15 +0900

    비밀번호 재발급 페이지 제작중 | req1-2 | S1P3151005-9

--- front/pages/ResetPassword.vue
+++ front/pages/ResetPassword.vue
@@ -11,24 +11,31 @@
                 <v-flex class="ivory text-left">비밀번호</v-flex>
                 <v-text-field
                   v-model="password"
-                  type="password"
-                  :rules="passRules"
+                  :type="passwordShow ? 'text' : 'password'"
+                  :rules="[rules.password, rules.minLength(8), rules.maxLength(20)]"
                   label="Password"
+                  :append-icon="passwordShow ? 'mdi-eye' : 'mdi-eye-off'"
+                  @click:append="passwordShow = !passwordShow"
                   solo
                   required
                 ></v-text-field>
                 <v-flex class="ivory text-left">비밀번호 확인</v-flex>
                 <v-text-field
-                  v-model="repassword"
-                  type="password"
-                  :rules="repassRules"
-                  label="Password"
+                  v-model="rePassword"
+                  :type="rePasswordShow ? 'text' : 'password'"
+                  :rules="[rules.password, rules.minLength(8), rules.maxLength(20)]"
+                  label="Confirm Password"
+                  :append-icon="rePasswordShow ? 'mdi-eye' : 'mdi-eye-off'"
+                  @click:append="rePasswordShow = !rePasswordShow"
                   solo
                   required
                 ></v-text-field>
               </v-form>
-              <v-flex class="ivory">{{message}}</v-flex>
-              <v-btn block class="backivory" @click="validate">확인</v-btn>
+              <v-flex class="mb-3" v-if="password.length >= 8 && rePassword.length >= 8">
+                <small class="text-success" v-if="password === rePassword">비밀번호가 일치합니다.</small>
+                <small class="text-warning" v-else>비밀번호가 일치하지 않습니다.</small>
+              </v-flex>
+              <v-btn block class="backivory">확인</v-btn>
             </v-card-text>
             <v-card-actions class="text-xs-center">
               <v-btn color="error" nuxt to="/">홈으로</v-btn>
@@ -47,25 +54,24 @@ export default {
   components: {
     auth
   },
-  data:() => ({
-    title: 'Email Authentification',
-    message: '',
-    authCompleted: true,
-    password: '',
-    passRules: [
-      (v) => !!v || 'Password를 입력해 주세요.',
-      (v) => (v && v.length >= 8) || '비밀번호는 최소 8자리입니다.'
-    ],
-    repassword: '',
-    repassRules: [
-
-    ]
-  }),
-  methods: {
-    validate() {
-      if(this.$refs.form.validate()) {
-        // 비밀번호 유효성 검사 완료
-        // 비밀번호 재설정 진행
+  data() {
+    return {
+      title: 'Email Authentification',
+      message: '',
+      messageType: '',
+      authCompleted: true,
+      password: '',
+      rePassword: '',
+      passwordShow: false,
+      rePasswordShow: false,
+      rules: {
+        password: (v) =>
+          /^(?=.*[a-z])(?=.*\d)(?=.*(_|[^\w])).+$/.test(v || '') ||
+          '비밀번호를 작성하여주세요. 비밀번호는 영문, 숫자, 특수문자를 포함하여야 합니다.',
+        minLength: (len) => (v) =>
+          (v || '').length >= len || `해당 내용은 ${len}자를 넘어야 합니다.`,
+        maxLength: (len) => (v) =>
+          (v || '').length <= len || `해당 내용은 ${len}자를 넘을 수 없습니다.`
       }
     }
   }
