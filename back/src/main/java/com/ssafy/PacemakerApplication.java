c1f924bdc3f554e753cad4c648453f0e3c60b06f
Baek, Yungi <phaethon5882@naver.com>
2019-10-15 10:55:46 +0900

    Setting backend | req0-2 | S1P3151005-40

---
+++ back/src/main/java/com/ssafy/PacemakerApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacemakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PacemakerApplication.class, args);
+    }
+
+}
