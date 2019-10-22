e6346ccc1cf71990b57b90d17f8f7956ea655fa3
송병하 <qudgk0006@naver.com>
2019-10-22 17:58:11 +0900

    Setting backend | req0-2 | S1P3151005-40

--- back/src/main/java/com/ssafy/controller/UserCtroller.java
+++ back/src/main/java/com/ssafy/controller/UserCtroller.java
@@ -1,14 +1,18 @@
 package com.ssafy.controller;
 
+import com.ssafy.exception.ResourceNotFoundException;
 import com.ssafy.model.User;
 import com.ssafy.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -29,6 +33,27 @@ public class UserCtroller {
     }
 
 
+    @PutMapping("/users")
+    public ResponseEntity<User> updatePass(@Valid @RequestBody User roomDetails) throws ResourceNotFoundException {
+        Long userId = roomDetails.getId();
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + userId));
+        user.setPassword(roomDetails.getPassword());
+        final User updatedUser = userRepository.save(user);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public Map<String, Boolean> deleteUser(@PathVariable(value = "id") Long userId)
+            throws ResourceNotFoundException {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + userId));
+
+        userRepository.delete(user);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
+    }
 
 
     public static String sha256(String msg) throws NoSuchAlgorithmException {
