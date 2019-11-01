e88fb9427e5b8244dd1984ccfc6089bf415db664
송병하 <qudgk0006@naver.com>
2019-11-01 14:31:15 +0900

    Add comment | req 3-2 | S1P3151005-53

--- back/src/main/java/com/ssafy/repository/RoomListRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.repository;
-
-import com.ssafy.model.RoomList;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomListRepository extends JpaRepository<RoomList,Long> {
-}
