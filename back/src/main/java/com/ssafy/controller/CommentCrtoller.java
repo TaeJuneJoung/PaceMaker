5ee8691d6f1935efdf5321aed943fb1635522049
songbyeongha <qudgk0006@naver.com>
2019-11-01 21:33:25 +0900

    Modify Comment

--- back/src/main/java/com/ssafy/controller/CommentCrtoller.java
+++ back/src/main/java/com/ssafy/controller/CommentCrtoller.java
@@ -1,11 +1,13 @@
 package com.ssafy.controller;
 
 import com.ssafy.model.Comment;
+import com.ssafy.model.CommetInput;
 import com.ssafy.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -25,7 +27,12 @@ public class CommentCrtoller {
     }
 
     @PostMapping("/comments")
-    public Comment createComments(@Valid @RequestBody Comment comment) {
+    public Comment createComments(@Valid @RequestBody CommetInput commentInput) {
+        Comment comment = new Comment();
+        comment.setNickname(commentInput.getNickname());
+        comment.setModelRoomId(commentInput.getModelRoomId());
+        comment.setContext(commentInput.getContext());
+        comment.setDate(new Date());
         return commentRepository.save(comment);
     }
 }
