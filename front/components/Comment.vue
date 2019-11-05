d428a3450489a9d79611e1d93fdcd1cafdffd1c9
TaeJuneJoung <jtj0525@gmail.com>
2019-11-05 22:51:09 +0900

    Modify Comment

--- front/components/Comment.vue
+++ front/components/Comment.vue
@@ -10,7 +10,7 @@
     class="commet"
     @click:append-outer="sendMessage"
     @click:clear="clearMessage"
-		@keyup.enter="sendMessage"
+    @keyup.enter="sendMessage"
   ></v-text-field>
 </template>
 
@@ -37,35 +37,39 @@ export default {
         modelRoomId: this.getModelRoomId,
         context: this.message
       }
-      createComment(commentData)
+      await createComment(commentData)
         .then(({ data }) => {
+          this.$store.dispatch(
+            'comment/setCommentList',
+            commentData.modelRoomId
+          )
         })
         .catch((error) => {
           console.error(error)
         })
-        this.$store.dispatch('comment/setCommentList', commentData.modelRoomId)
-        let achieveData = {}
-        await getAchieve(this.$session.get('account').id).then(({ data }) => {
-          achieveData = data
-        })
-        achieveData.comment += 1
-        await putAchieve(achieveData)
 
-        const achieveModal = this.$store.state.achievement.commentAchieve;
-        achieveModal.forEach((element) => {
-          if (element.number == achieveData.comment) {
-            this.$store.commit('modal/setModalData', {
-              header: element.name,
-              body: '댓글 등록 성공!! \n 업적을 취득하였습니다.',
-              img: element.img
-            })
-            this.$store.commit('achievement/setShowModal', true)
-          }
-        })
-        let elem = document.getElementById('scroll-content')
-        let container = document.getElementById('scroll-target')
-        container.scrollTop = Math.floor(elem.offsetHeight)
-        this.clearMessage()
+      let achieveData = {}
+      await getAchieve(this.$session.get('account').id).then(({ data }) => {
+        achieveData = data
+      })
+
+      achieveData.comment += 1
+      await putAchieve(achieveData)
+
+      const achieveModal = this.$store.state.achievement.commentAchieve
+      achieveModal.forEach((element) => {
+        if (element.number == achieveData.comment) {
+          this.$store.commit('modal/setModalData', {
+            header: element.name,
+            body: '댓글 등록 성공!! \n 업적을 취득하였습니다.',
+            img: element.img
+          })
+          this.$store.commit('achievement/setShowModal', true)
+        }
+      })
+      this.clearMessage()
+      let container = document.getElementById('scroll-target')
+      container.scrollTop = container.scrollHeight
     },
     clearMessage() {
       this.message = ''
